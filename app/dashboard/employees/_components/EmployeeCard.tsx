f0ec8965b1d070f16d6ad541f046ae1cfb2c4589
SebastianMtzG <martinezgarciasebastian874@gmail.com>
2024-11-14 22:54:32 -0600

    EmployeeCar y EmployeePhotoCard

---
+++ app/dashboard/employees/_components/EmployeeCard.tsx
@@ -0,0 +1,25 @@
+
+import { Card, Button, CardHeader, Divider, CardBody, CardFooter } from "@nextui-org/react"
+import { Employee } from "@/entities"
+import Link from "next/link"
+
+export default function EmployeeCard({employee} : { employee: Employee}){
+    return (
+        <Card  className="size-72 max-h-72">
+            <CardHeader >
+             <h1 className="font-bold text-xl">  {employee.employeeName + " " + employee.employeeLastname}</h1> 
+            </CardHeader>
+       
+        <Divider/>
+        <CardBody>
+        <p>Correo: <b>{employee.employeeEmail}</b></p>
+        <p>Número de teléfono: <b>{employee.employeePhonenumber}</b></p>
+        </CardBody>
+        <CardFooter className="absolute bottom-0 py-2 h-14">
+        <Link href={`/dashboard/employees/${employee.employeeId}`}>
+        <Button variant="ghost">Actualizar</Button>
+        </Link>
+        </CardFooter>
+        </Card>
+    )
+}
---
+++ app/dashboard/employees/_components/EmployeePhotoCard.tsx
@@ -0,0 +1,23 @@
+
+import { Card, CardHeader, Divider, Image, CardFooter } from "@nextui-org/react"
+import { Employee } from "@/entities"
+import { Button } from "@nextui-org/react"
+import Link from "next/link"
+
+export default function EmployeePhotoCard({employee} : { employee: Employee}){
+    return (
+        <Card className="max-h-72">
+            <CardHeader className="absolute top-0 bg.black bg-opacity-25 z-10"> 
+             <h1 className="font-bold text-xl text-white drop-shadow-sm">  {employee.employeeName + " " + employee.employeeLastname}</h1>     
+            </CardHeader>
+       
+        <Divider/>
+        <Image src={employee.employeePhoto} className="z-0" classNames={{img:"size-72"}} />
+        <CardFooter className="absolute bottom-0 h-14">
+        <Link href={`/dashboard/employees/${employee.employeeId}`}>
+        <Button variant="ghost">Actualizar</Button>
+        </Link>
+        </CardFooter>
+        </Card>
+    )
+}
